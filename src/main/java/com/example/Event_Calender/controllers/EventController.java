9a914253897540ce5fe15b4f7193a3a6200215bb
rimmderes <rimmderes@gmail.com>
2022-12-12 13:25:30 +0000

    Completed Event and User Service class

--- src/main/java/com/example/Event_Calender/controllers/EventController.java
+++ src/main/java/com/example/Event_Calender/controllers/EventController.java
@@ -1,4 +1,13 @@
 package com.example.Event_Calender.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/events")
 public class EventController {
+
+
 }
--- src/main/java/com/example/Event_Calender/services/EventService.java
+++ src/main/java/com/example/Event_Calender/services/EventService.java
@@ -1,4 +1,52 @@
 package com.example.Event_Calender.services;
 
+import com.example.Event_Calender.models.Event;
+import com.example.Event_Calender.models.User;
+import com.example.Event_Calender.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class EventService {
+
+    @Autowired
+    EventRepository eventRepository;
+
+    @Autowired
+    UserService userService;
+
+    public List<Event> getAllEvents(){
+        return eventRepository.findAll();
+    }
+
+    public Event getEventById(long id) {
+        return eventRepository.findById(id).get();
+    }
+
+    public Event addNewEvent(Event event){
+        eventRepository.save(event);
+        return event;
+    }
+
+    public Event addUserToEvent(long eventId, long userId) {
+        // instantiating an event that already exists in the database
+        Event event = eventRepository.findById(eventId).get();
+        // instantiating a user utilising their id
+        User user = userService.getUserById(userId);
+        // instantiating users into getUsers event method
+        List <User> users = event.getUsers();
+        users.add(user);
+        // setting users with events
+        event.setUsers(users);
+        eventRepository.save(event);
+        return event;
+    }
+
+    public void deleteEvent(long id) {
+        eventRepository.deleteById(id);
+    }
+
+
 }
