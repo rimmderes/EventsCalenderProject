314dbcc7b7da104afa9361c80b492bd672b35389
rimmderes <rimmderes@gmail.com>
2022-12-12 09:46:16 +0000

    First Commit

---
+++ src/main/java/com/example/Event_Calender/EventCalenderApplication.java
@@ -0,0 +1,13 @@
+package com.example.Event_Calender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventCalenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventCalenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Event_Calender/EventCalenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Event_Calender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventCalenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
