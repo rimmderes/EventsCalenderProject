e44558011a8a0e0af12e71356a4d2b85e13b7e0c
rimmderes <rimmderes@gmail.com>
2023-01-27 16:53:57 +0000

    cors included

---
+++ src/main/java/com/example/Event_Calender/configurations/SpringGlobalConfig.java
@@ -0,0 +1,14 @@
+package com.example.Event_Calender.configurations;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class SpringGlobalConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings (CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*");
+    }
+}
